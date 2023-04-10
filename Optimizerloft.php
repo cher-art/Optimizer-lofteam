00a54d8249ba435889f0f5e34aa729c05c23b92c
Artem Chernyshow <artemchernyshow@MacBook-Pro.local>
2023-04-10 19:42:56 +0300

    New project

---
+++ Optimizerloft.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Loft Optimizer
+Plugin URI: http://optimizerLoft.com
+Description: Наш первый плагин по оптимизации изображений на wordpress.
+Version: 1.0
+Author: Basicteam
+Author URL: http://basicteam.com
+ */
