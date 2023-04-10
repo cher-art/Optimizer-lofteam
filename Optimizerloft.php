c0361a2186797d4a30c94010e8050f1545c7a066
artemchernyshow <cher.tim@gmail.com>
2023-04-10 21:42:45 +0300

    Add block

--- Optimizerloft.php
+++ Optimizerloft.php
@@ -4,12 +4,8 @@ Plugin Name: Loft Optimizer
 Plugin URI: http://optimizerLoft.com
 Description: Наш первый плагин по оптимизации изображений на wordpress.
 Version: 1.0
-Author: Basicteam
-Author URL: http://basicteam.com
+Author: Basilteam
+Author URL: http://Basilteam.com
  */
 
-function add_new_word() {
-    echo "Hello World123123";
-}
-
-add_action('admin_init', 'add_new_word');
+defined('ABSPATH') or die();
